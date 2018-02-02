f6e8f55e0f423bd870483c23dace08999d469c6e
Jeffrey Gillis <jgillis@optoro.com>
2018-02-02 11:30:17 -0500

    Use GenServer to store worker instance state

---
+++ lib/worker_tracker/server.ex
@@ -0,0 +1,20 @@
+defmodule WorkerTracker.Server do
+  use GenServer
+
+  alias WorkerTracker.WorkerInstance
+
+  def init(worker) do
+    worker_instance = WorkerInstance.from_instance_name(worker)
+    {:ok, worker_instance}
+  end
+
+  def handle_call(:get_instance, _from, worker_instance) do
+    {:reply, worker_instance, worker_instance}
+  end
+
+  def handle_cast(:refresh_processes, worker_instance) do
+    worker_instance = WorkerInstance.refresh_processes(worker_instance)
+    {:noreply, worker_instance}
+  end
+
+end
