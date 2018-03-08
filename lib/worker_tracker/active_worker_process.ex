c87b543b1ef3bccdda386979c177bac348954ba4
Jeffrey Gillis <jgillis@optoro.com>
2018-03-08 17:29:57 -0500

    Remove variable data from struct creation

--- lib/worker_tracker/active_worker_process.ex
+++ lib/worker_tracker/active_worker_process.ex
@@ -2,9 +2,8 @@ defmodule WorkerTracker.ActiveWorkerProcess do
   defstruct(
     owner: "",
     pid: 0,
-    since_time: nil,
     name: "",
-    duration: 0
+    start_time_seconds: 0
   )
 
   alias WorkerTracker.{ActiveWorkerProcess, ProcessHelper}
@@ -15,9 +14,8 @@ defmodule WorkerTracker.ActiveWorkerProcess do
   ## Example
 
       iex> process_string = "deploy 12345 a b c d e f g h i j k l 1516831492 [some_worker_process]"
-      iex> worker_process = WorkerTracker.ActiveWorkerProcess.parse_worker_process(process_string)
-      iex> worker_process.name == "some_worker_process"
-      true
+      iex> WorkerTracker.ActiveWorkerProcess.parse_worker_process(process_string)
+      %WorkerTracker.ActiveWorkerProcess{owner: "deploy", pid: 12345, start_time_seconds: 1516831492, name: "some_worker_process"}
   """
   def parse_worker_process(process_string) do
     process_string
@@ -37,13 +35,8 @@ defmodule WorkerTracker.ActiveWorkerProcess do
   end
 
   defp build_worker_process({value, 14}, worker_process) do
-    {:ok, datetime} =
-      value
-      |> String.to_integer()
-      |> DateTime.from_unix()
-
-    duration = DateTime.diff(DateTime.utc_now(), datetime)
-    %{worker_process | since_time: datetime, duration: duration}
+    value = value |> String.to_integer()
+    %{worker_process | start_time_seconds: value}
   end
 
   defp build_worker_process({value, 15}, worker_process) do
