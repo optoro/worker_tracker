90681f781c355ddea3493979fb54feb6a68cfe95
Jeffrey Gillis <jgillis@optoro.com>
2018-03-13 16:33:12 -0400

    Revert Supervisor work

--- lib/worker_tracker.ex
+++ lib/worker_tracker.ex
@@ -1,26 +1,19 @@
 defmodule WorkerTracker do
+  alias WorkerTracker.Server
 
-  def start_link(worker) do
-    Supervisor.start_link(WorkerTracker.Supervisor, worker)
+  def new(worker) do
+    GenServer.start_link(Server, worker)
   end
 
   def get_instance(worker_pid) do
-    pid = get_child_pid(worker_pid)
-    GenServer.call(pid, :get_instance)
+    GenServer.call(worker_pid, :get_instance)
   end
 
   def refresh_processes(worker_pid) do
-    pid = get_child_pid(worker_pid)
-    GenServer.cast(pid, :refresh_processes)
+    GenServer.cast(worker_pid, :refresh_processes)
   end
 
   def terminate_process(worker_pid, process_id, use_sudo) do
-    pid = get_child_pid(worker_pid)
-    GenServer.cast(pid, {:terminate_process, process_id, use_sudo})
-  end
-
-  defp get_child_pid(sup_pid) do
-    [{module, child_pid, :worker, [module]}] = Supervisor.which_children(sup_pid)
-    child_pid
+    GenServer.cast(worker_pid, {:terminate_process, process_id, use_sudo})
   end
 end
