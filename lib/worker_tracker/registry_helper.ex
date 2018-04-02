865015adcaa4c79cbab6209e696f301083ac1e7b
Jeffrey Gillis <jgillis@optoro.com>
2018-04-02 16:42:21 -0400

    Update formatting via

--- lib/worker_tracker/registry_helper.ex
+++ lib/worker_tracker/registry_helper.ex
@@ -1,18 +1,16 @@
 defmodule WorkerTracker.RegistryHelper do
-
   def register(instance) do
     Registry.register(WorkerTracker.Registry, "workers", instance)
   end
 
   def lookup(instance) do
     Registry.lookup(WorkerTracker.Registry, "workers")
-    |> Enum.filter(fn({_pid, instance_name}) -> instance == instance_name end)
+    |> Enum.filter(fn {_pid, instance_name} -> instance == instance_name end)
     |> List.first()
   end
 
   def keys() do
     Registry.lookup(WorkerTracker.Registry, "workers")
-    |> Enum.map(fn({_pid, instance}) -> instance end)
+    |> Enum.map(fn {_pid, instance} -> instance end)
   end
-
 end
