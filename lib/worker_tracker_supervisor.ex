e6ddd90bfbcb67dbcc820f7b9e1d62c0162470a4
Jeffrey Gillis <jgillis@optoro.com>
2018-08-22 11:37:57 -0400

    Bump max_restarts count

--- lib/worker_tracker_supervisor.ex
+++ lib/worker_tracker_supervisor.ex
@@ -11,8 +11,8 @@ defmodule WorkerTrackerSupervisor do
       {Registry, [keys: :duplicate, name: WorkerTracker.Notifier]},
       {Registry, [keys: :unique, name: WorkerTracker.InstanceRegistry]},
       {Registry, [keys: :unique, name: WorkerTracker.WorkerRegistry]},
-      {DynamicSupervisor, name: InstanceSupervisor, strategy: :one_for_one},
-      {DynamicSupervisor, name: WorkerSupervisor, strategy: :one_for_one},
+      {DynamicSupervisor, name: InstanceSupervisor, strategy: :one_for_one, max_restarts: 100},
+      {DynamicSupervisor, name: WorkerSupervisor, strategy: :one_for_one, max_restarts: 100},
       {Task.Supervisor, name: WorkerTracker.TaskSupervisor},
       {WorkerServer, []}
     ]
