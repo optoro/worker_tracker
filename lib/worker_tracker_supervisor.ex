66b26ef2702b2531cd6eeb20db8aaf40a3399b62
Jeffrey Gillis <jgillis@optoro.com>
2018-08-23 11:38:16 -0400

    Update max_restarts value for dynamic supervisors

--- lib/worker_tracker_supervisor.ex
+++ lib/worker_tracker_supervisor.ex
@@ -12,8 +12,8 @@ defmodule WorkerTrackerSupervisor do
       {Registry, [keys: :duplicate, name: WorkerTracker.CollectionRegistry]},
       {Registry, [keys: :unique, name: WorkerTracker.InstanceRegistry]},
       {Registry, [keys: :unique, name: WorkerTracker.WorkerRegistry]},
-      {DynamicSupervisor, name: InstanceSupervisor, strategy: :one_for_one},
-      {DynamicSupervisor, name: WorkerSupervisor, strategy: :one_for_one},
+      {DynamicSupervisor, name: InstanceSupervisor, strategy: :one_for_one, max_restarts: 100},
+      {DynamicSupervisor, name: WorkerSupervisor, strategy: :one_for_one, max_restarts: 100},
       {Task.Supervisor, name: WorkerTracker.TaskSupervisor},
       {WorkerServer, []}
     ]
