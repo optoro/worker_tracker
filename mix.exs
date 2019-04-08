643bd2e0981bc5ac72170db3309d3be0e16adaf6
Spencer Gilbert <sgilbert@optoro.com>
2019-04-08 18:51:23 -0400

    Add describe and package function to project

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule WorkerTracker.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
