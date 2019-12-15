ea42ee7416708d61667fc1044bd1abdba3c47e86
venkyj <vjeyanthilal@optoro.com>
2019-12-14 21:59:11 -0500

    Support resque pool master process information
    We ran into an issue where multiple resque pool processes
    were running on the same worker machine.
    Adding support to look for the resque pool master process and
    getting the list of processes managed by it.

--- lib/worker_tracker/worker_instance/client.ex
+++ lib/worker_tracker/worker_instance/client.ex
@@ -1,21 +1,23 @@
 defmodule WorkerTracker.WorkerInstance.Client do
-  defstruct name: "", active_workers: [], waiting_workers: []
+  defstruct name: "", active_workers: [], waiting_workers: [], resque_pool_masters: []
 
   alias WorkerTracker.WorkerInstance.ActiveWorkerProcess
   alias WorkerTracker.WorkerInstance.WaitingWorkerProcess
+  alias WorkerTracker.WorkerInstance.ResquePoolMasterProcess
   alias WorkerTracker.ProcessHelper
 
   def refresh_instance(%__MODULE__{} = worker_instance) do
     worker_instance
     |> get_all_processes()
+    |> parse_resque_pool_masters(worker_instance)
     |> parse_active_workers(worker_instance)
     |> parse_waiting_workers(worker_instance)
-
     :ok
   end
 
   def get_instance(%__MODULE__{} = worker_instance) do
     worker_instance
+    |> Map.put(:resque_pool_masters, retrieve_worker_registrations(worker_instance, "master"))
     |> Map.put(:active_workers, retrieve_worker_registrations(worker_instance))
     |> Map.put(:waiting_workers, retrieve_worker_registrations(worker_instance, "waiting"))
   end
@@ -36,6 +38,16 @@ defmodule WorkerTracker.WorkerInstance.Client do
     |> ProcessHelper.create_list_from_string()
   end
 
+  defp parse_resque_pool_masters(processes, %__MODULE__{} = worker_instance) do
+    processes
+    |> ProcessHelper.filter_and_transform_process_list(
+         "resque-pool-master",
+         &ResquePoolMasterProcess.parse_master_process/1
+       )
+    |> update_worker_registrations(worker_instance.name, "master")
+    processes
+  end
+
   defp parse_active_workers(processes, %__MODULE__{} = worker_instance) do
     processes
     |> ProcessHelper.filter_and_transform_process_list(
@@ -43,7 +55,6 @@ defmodule WorkerTracker.WorkerInstance.Client do
       &ActiveWorkerProcess.parse_worker_process/1
     )
     |> update_worker_registrations(worker_instance.name)
-
     processes
   end
 
